777401e10ec663d65a7f0558b71a0eee5e8c80c9
PartoTheEagle <asalhaytham@gmail.com>
2016-08-27 18:42:35 +0100

    Added costum message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,7 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connect to socket.io server!');
 });
+
+socket.on('message', function (message){
+	console.log(`New message:  ${message.text}`);
+});
